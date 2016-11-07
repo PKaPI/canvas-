708b5f096ccc99f816cbb585eb9e80ea4b3445eb
皮振 <zhen.pi@jsxfedu.com>
2016-11-07 16:27:03 +0800

    增加注释LAnimationTimeline的动画帧

--- js/Flashs.js
+++ js/Flashs.js
@@ -5,7 +5,7 @@ function Flashs(tempx,tempy,val){
 	self.value=val;
 	var list=LGlobal.divideCoordinate(870,145,1,6);
 	var data=new LBitmapData(imglist["weapon"],0,0,145,145);
-	flash=new LAnimationTimeline(data,list);
+	flash=new LAnimationTimeline(data,list);//LAnimationTimeline对象不用调用onfram方法了
 	flash.speed=3;
 	flash.x=-flash.getWidth()/2;
 	flash.y=-flash.getHeight()/2;
